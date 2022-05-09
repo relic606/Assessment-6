ea11fbb93d1aad6de840c2cc18354b9982529114
Andy Anderson <relic606@yahoo.com>
2022-05-08 18:02:19 -0600

    Added unit tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,18 @@
 const {shuffleArray} = require('./utils')
 
+let arr = [1,2,3,4]
+
+console.log(typeof arr)
+console.log(typeof shuffleArray(arr))
+
+console.log(arr.length)
+console.log(shuffleArray(arr).length)
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    it('should return an object', () => {
+        expect(typeof shuffleArray(arr)).toBe(typeof arr)
+    })
+    it('should be same length as original object', () => {
+        expect(shuffleArray(arr).length).toBe(arr.length)
+    })
 })
